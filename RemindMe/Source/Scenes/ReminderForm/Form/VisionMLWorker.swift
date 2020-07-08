e4be1343bc5f44596ee17a6e9545f055cd483b18
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-09 00:05:36 +0530

    Added worker class to process image using ML

---
+++ RemindMe/Source/Scenes/ReminderForm/Form/VisionMLWorker.swift
@@ -0,0 +1,22 @@
+//
+//  VisionMLWorker.swift
+//  RemindMe
+//
+//  Created by Ajith R Nayak on 08/07/20.
+//  Copyright © 2020 ajithrnayak. All rights reserved.
+//
+
+import Foundation
+import Vision
+
+class VisionMLWorker {
+    
+    /*
+     Use the Swift class `MobileNetV2` Core ML generates from the model.
+     To use a different Core ML classifier model, see
+     https://developer.apple.com/machine-learning/models/
+     */
+    lazy var classificationRequest: VNCoreMLRequest = {
+       
+    }()
+}
