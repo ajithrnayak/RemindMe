b12551ed84f894eb7c874f8e5336cf16f8d997d9
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-08 11:00:45 +0530

    Added a create reminder button

--- RemindMe/Source/Scenes/Reminders/RemindersVC.swift
+++ RemindMe/Source/Scenes/Reminders/RemindersVC.swift
@@ -9,26 +9,38 @@
 import UIKit
 
 class RemindersVC: UIViewController {
-
+    
     private let remindersList = RemindersListVC.newInstance()
+    
     private let containerView: UIView = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         return $0
     }(UIView())
     
+    private let createReminderButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .systemBlue
+        let boldConfiguration = UIImage.SymbolConfiguration(weight: .bold)
+        let boldSymbolImage = UIImage(systemName: "plus.circle.fil",
+                                      withConfiguration: boldConfiguration)
+        $0.imageView?.image = boldSymbolImage?.withTintColor(.cyan,
+                                                             renderingMode: .alwaysOriginal)
+        return $0
+    }(UIButton(type: .custom))
+    
     // MARK: - View Life Cycle
-
+    
     override func loadView() {
         super.loadView()
         setupRemindersList()
+        setupCreateReminderButton()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
         title = "RemindMe"
     }
-
+    
     // MARK: - Initial Configuration
     private func setupRemindersList() {
         view.addSubview(containerView)
@@ -36,6 +48,17 @@ class RemindersVC: UIViewController {
         
         containerView.addSubview(remindersList.view)
         remindersList.view.addConstraintsToMatch(superView: containerView)
+    }
+    
+    private func setupCreateReminderButton() {
+        view.addSubview(createReminderButton)
+        let constraints = [createReminderButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                                                        constant: -8.0),
+                           createReminderButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                                          constant: -16.0),
+                           createReminderButton.heightAnchor.constraint(equalToConstant: 50.0),
+                           createReminderButton.widthAnchor.constraint(equalToConstant: 50.0)]
+        constraints.forEach { $0.isActive = true }
     }
 }
 
