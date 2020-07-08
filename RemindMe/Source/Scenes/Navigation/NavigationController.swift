7d78ed2f32600cc19ae2434db26888db74c9df08
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-08 10:00:11 +0530

    Added a navigation controller

---
+++ RemindMe/Source/Scenes/Navigation/NavigationController.swift
@@ -0,0 +1,24 @@
+//
+//  NavigationController.swift
+//  RemindMe
+//
+//  Created by Ajith R Nayak on 08/07/20.
+//  Copyright © 2020 ajithrnayak. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Factory Initializer
+extension NavigationController {
+    class func newInstance(using rootVC: UIViewController) -> NavigationController {
+        let navController = NavigationController(rootViewController: rootVC)
+        return navController
+    }
+}
