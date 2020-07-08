e2a69007a0b7e04fdbfdd0ffe866756e5df6fca5
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-08 10:05:34 +0530

    implemented a landing screen

---
+++ RemindMe/Source/Scenes/Reminders/RemindersVC.swift
@@ -0,0 +1,27 @@
+//
+//  RemindersVC.swift
+//  RemindMe
+//
+//  Created by Ajith R Nayak on 08/07/20.
+//  Copyright © 2020 ajithrnayak. All rights reserved.
+//
+
+import UIKit
+
+class RemindersVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        title = "RemindMe"
+    }
+
+}
+
+// MARK: - Factory Initializer
+extension RemindersVC {
+    class func newInstance() -> RemindersVC {
+        let remindersVC = RemindersVC()
+        return remindersVC
+    }
+}
