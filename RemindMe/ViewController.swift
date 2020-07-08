af5f8f6d3c1b2722021355e7a89afcd7d26448f7
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-08 09:02:53 +0530

    Initial Commit

---
+++ RemindMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemindMe
+//
+//  Created by Ajith R Nayak on 08/07/20.
+//  Copyright © 2020 ajithrnayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
