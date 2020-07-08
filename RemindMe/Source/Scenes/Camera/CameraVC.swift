b2c595c665001106391b183f82beff6927e9be09
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-08 11:55:13 +0530

    Added a container view for camera preview

--- RemindMe/Source/Scenes/Camera/CameraVC.swift
+++ RemindMe/Source/Scenes/Camera/CameraVC.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class CameraVC: UIViewController {
 
+    private let cameraFeedView: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIView())
+    
+    // MARK: - View Life cycle
+    override func loadView() {
+        super.loadView()
+        setupCameraFeed()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
         // Do any additional setup after loading the view.
     }
     
+    // MARK: - Initial Setup
+    private func setupCameraFeed() {
+        view.addSubview(cameraFeedView)
+        cameraFeedView.addConstraintsToMatch(superView: view)
+    }
+
 }
 
 // MARK: - Factory Initializer
