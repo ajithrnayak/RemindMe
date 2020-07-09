572036365e270d698fcfd7646a8c5ce1500552e1
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-09 16:54:13 +0530

    Missed app icons in previous commit

--- RemindMe/Source/Helpers/Styles/AppIcons.swift
+++ RemindMe/Source/Helpers/Styles/AppIcons.swift
@@ -11,16 +11,31 @@ import UIKit
 
 enum AppIcons {
     case createNew
+    case notifyOn
+    case notifyOff
     
     var image: UIImage? {
+        let boldConfiguration = UIImage.SymbolConfiguration(weight: .bold)
+        
         switch self {
         case .createNew:
-            let boldConfiguration = UIImage.SymbolConfiguration(weight: .bold)
             let boldSymbolImage = UIImage(systemName: "plus.circle.fill",
                                           withConfiguration: boldConfiguration)
             let plusImage = boldSymbolImage?.withTintColor(.white,
                                                            renderingMode: .alwaysOriginal)
             return plusImage
+        case .notifyOn:
+            let boldSymbolImage = UIImage(systemName: "bell.fill",
+                                          withConfiguration: boldConfiguration)
+            let bellImage = boldSymbolImage?.withTintColor(.black,
+                                                           renderingMode: .alwaysOriginal)
+            return bellImage
+        case .notifyOff:
+            let boldSymbolImage = UIImage(systemName: "bell.slash.fill",
+                                          withConfiguration: boldConfiguration)
+            let bellImage = boldSymbolImage?.withTintColor(.black,
+                                                           renderingMode: .alwaysOriginal)
+            return bellImage
         }
     }
 }
