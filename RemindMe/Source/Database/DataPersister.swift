4551a7fdb711d15c8a11256a3b16d3079b0733c1
Ajith R Nayak <ajithrnayak@icloud.com>
2020-07-09 23:07:01 +0530

    implemented core data stack

--- RemindMe/Source/Database/DataPersister.swift
+++ RemindMe/Source/Database/DataPersister.swift
@@ -7,7 +7,55 @@
 //
 
 import Foundation
+import CoreData
 
 class DataPersister {
+    /// Shared access for data persistence
+    static let shared: DataPersister = DataPersister()
+    private let defaultFetchBatchSize = 50
     
+    // MARK: - initializer
+    private init() { }
+    
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "RemindMe")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            Log.info("Loading persistent store \(storeDescription)")
+            if let error = error as NSError? {
+                Log.fatal("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    /// Shortcut to access main managed object context
+    var mainContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    var newBackgroundContext: NSManagedObjectContext {
+        return persistentContainer.newBackgroundContext()
+    }
+    
+    var managedObjectModel: NSManagedObjectModel {
+        return persistentContainer.managedObjectModel
+    }
+
+       // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            }
+            catch {
+                let nserror = error as NSError
+                Log.fatal("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
 }
